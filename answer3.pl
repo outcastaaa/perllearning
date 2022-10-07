ce17d16b3cddf20afd7614919d5ba951053bae39
outcastaaa <1191474049@qq.com>
2022-10-07 09:12:47 +0800

    1

---
+++ answer3.pl
@@ -0,0 +1,17 @@
+#第四章
+
+#!user/bin/perl
+use warnings;
+use v5.34;
+
+$fred = 3;
+$barney =4;
+use warnings;
+sub sum{
+    print "you called the sum!\n";
+    $fred + $barney;
+    print "im returning a value.\n";
+}
+
+&sum;
+
