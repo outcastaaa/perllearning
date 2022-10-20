a4b16b5759ca40320f596065fd200ad76b4656a2
outcastaaa <1191474049@qq.com>
2022-10-20 22:14:24 +0800

    1

--- answer9.pl
+++ answer9.pl
@@ -55,12 +55,25 @@ foreach $_(@fields){
 #
 #sdfe
 
+my @fields = split /\s+/, " this   is .   a blank .\n";
+foreach $_(@fields){
+    print "$_\n";
+}
+#
+#this
+#is
+#.
+#a
+#blank
+#.
+
+
 
 #join
 @_=qw"4 6 8 10 12";
-my $x = join ":", @_;
+my $x = join ":", @_;  #先用胶水连接起来
 print "$x\n";  #4:6:8:10:12
-my @values = split ":", $x;
+my @values = split ":", $x; 
 foreach(@values){
     print "$_\n";
 }
@@ -69,6 +82,9 @@ foreach(@values){
 #8
 #10
 #12
+my $z = join "-", @values; #再用-连接起来
+print "$z\n";  #4-6-8-10-12
+
 
 $_="hello there, boy.\n";
 my ($first, $second, $third) = /(\S+) (\S+), (\S+)/;
@@ -77,15 +93,16 @@ print "$second is my $third\n";
 
 
 #将匹配结果放在数组中
-my $text = "fred dropped a 5 ton grantie block on Mr. Slate";
-my @words = ($text =~ /([a-z]+)/ig);   #()可加可不加，但是更好的显示哪一部分被圈出来
+my $text = "Fred dropped a 5 ton grantie block on Mr. Slate";
+my @words = ($text =~ /([a-z]+)/g);   #()可加可不加，但是更好的显示哪一部分被圈出来
 print "results: @words\n";
-#results: fred dropped a ton grantie block on Mr Slate
+#results: red dropped a ton grantie block on r late
+
 
 #将成对匹配结果放在哈希中
 my $text = "barney rubble fred flintstone wilma flintstone";
-#my %name = ($text =~ /(?=(\w+)\s+(\w+))/g);、
-my %name = ($text =~ /(\w+)\s+(\w+)/g);
+#my %name = ($text =~ /(?=(\w+)\s+(\w+))/g);
+my %name = ($text =~ /(\w+)\s+(\w+)/g); #g的全局匹配具有唯一性，如果匹配过一次，下一次就从该词后面开始
 my @key = keys %name;
 my @value = values %name;
 
@@ -93,6 +110,7 @@ print "this is keys.\n";
 foreach(@key){
     print "$_\n";
 }
+
 print "this is values.\n";
 foreach(@value){
     
@@ -102,18 +120,92 @@ foreach(@value){
 #barney fred wilma
 #rubble flintstone flintstone
 
+
+
 #非贪婪词
+my $text = '<b>Fred</b> and <b>Barney</b>';
+(my $match_count = $text) =~ s|<b>(.*)</b>|\U$1|g;
+print "$match_count : $text\n";
+#FRED</B> AND <B>BARNEY : <b>Fred</b> and <b>Barney</b>  
+#$text变量不变，将内容copy给$match_count，并用$match_count做替换
+
+my $text = '<b>Fred</b> and <b>Barney</b>';
+my $match_count = $text =~ s|<b>(.*)</b>|\U$1|g;
+print "$match_count : $text\n";
+#1 : FRED</B> AND <B>BARNEY
+#这里的$match_count就是替换的数目
+
 my $text = '<b>Fred</b> and <b>Barney</b>';
 my $match_count = $text =~ s|<b>(.*?)</b>|\U$1|g;
 print "$match_count : $text\n";
 #2 : FRED and BARNEY
 
 
-#跨行模式匹配
 
+#单词边界符号
+my $string = "This donesn't capitalize correctly.";
+$string =~ s/\b(\w)/\U$1/g;
+print "$string\n";
+#This Donesn'T Capitalize Correctly.
+
+my $string = "This donesn't capitalize correctly.";
+$string =~ s/\s\b(\w)/ \U$1/g;
+print "$string\n";
+#This Donesn't Capitalize Correctly.
+
+my $string = "This donesn't capitalize correctly.";
+$string =~ s/\b{wb}(\w)/\U$1/g;
+print "$string\n";
+#This Donesn't Capitalize Correctly.
+
+
+#跨行模式匹配
 $_ = "im much better\nthan Barney is\nat bowling,\nwilma.\n";
+print "$_";
+#im much better
+#than Barney is
+#at bowling,
+#wilma.
 if (/^wilma\b/im){
-    print "found 'wilma' at start of line\n";
+    print "found 'wilma' at start of line.\n";
+}
+#found 'wilma' at start of line.
+
+
+
+
+use v5.34;
+
+#183-1:
+my $what = 'fred|barney';
+chomp($_ = <>);
+if ($_ =~ m/($what){3}/){
+    print "this is $&\n";
+}else{
+    print "can't found $what.\n";
 }
 
+#183-2:
 
+#cat dino
+#NAME : Manfred Mann
+#NAME : Manfred Mann
+#NAME111 : Manfred Mann
+
+#命令行：perl -p -i.bak -w -e 's/fred/Larry/igm' dino
+#执行后dino：
+#NAME : ManLarry Mann
+#NAME : ManLarry Mann
+#NAME111 : ManLarry Mann
+#执行后 dino.bak:
+#NAME : Manfred Mann
+#NAME : Manfred Mann
+#NAME111 : Manfred Mann
+
+#183-3：
+#cat file
+#name: wilma
+#name2:fred
+#name3:wilma fred
+
+#perl -p -i.bak -w -e 's/fred/Wilma/igm && s/wilma/Fred/igm' file
