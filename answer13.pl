b1558437b704a7464133088e8896b0eaff516793
outcastaaa <1191474049@qq.com>
2022-10-30 23:11:30 +0800

    Update answer13.pl

--- answer13.pl
+++ answer13.pl
@@ -16,11 +16,30 @@ foreach my $arg (@ARGV){
 # echo *.pl
 #123.pl answer10.pl answer11.pl answer12.pl answer13.pl answer2.pl answer231-1.pl answer3.pl answer4.pl answer5.pl answer6.pl answer7.pl answer8.pl answer9.pl duration.pl hello.pl use warnings;.pl
 
-
-
+#目录句柄没有试验成功
 
 
 
 #批量删除文件，删除以 .bak 结尾的备份文件
-my $success = unlink glob '*.bak';
-print "i deleted $success files.\n";
+#my $success = unlink glob '*.bak';
+#print "i deleted $success files.\n";
+#i deleted 14 files.
+
+
+
+
+#软硬连接
+#建立一个软链接/mnt/d/perl/perl$ ln -s 1.txt 1.1.txt
+my $where = readlink '1.1.txt';
+print "1.txt is in $where.\n";
+#1.txt is in 1.txt.
+
+my $perl = readlink '/user/bin/perl';
+print "perl is in $perl.\n"; 
+#perl is in .
+#不是链接，是本来的文件所以不会返回什么
+
+
+unlink '1.1.txt';
+#1.1.txt就被删除了
+
